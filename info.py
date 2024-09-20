cdff80f42c1e89b9f50a4604838d676dfc4e2a1f
Udayg3417 <udayg3417@gmail.com>
2024-09-20 12:23:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,20 +19,20 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 
 # Bot Information 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-ADMINS =int(os.environ.get("ADMINS", "")
+APP_ID = int(os.environ.get("APP_ID", "21562110"))
+API_HASH = os.environ.get("API_HASH", "4d1e1f333214a39cea79a3196cbed290")
+ADMINS =int(os.environ.get("ADMINS", "6909056240")
 ADMINS = environ.get("ADMINS", OWNER)
 
 #Channel 
 CHANNEL = int(os.environ.get("CHANNEL", ""))
-OWNER = int(os.environ.get("OWNER", ""))
+OWNER = int(os.environ.get("OWNER", "6909056240"))
 FORCE_CHANNEL = int(os.environ.get("FORCE_CHANNEL", "0"))
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
 # MongoDB Database 
 PORT = os.environ.get("PORT", "8080")
-DATABASE_URI = os.environ.get("DATABASE_URL", "")
+DATABASE_URI = os.environ.get("DATABASE_URL", "mongodb+srv://guday3417:GUDAY333@cluster0.pdrwfcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 # Other 
@@ -46,7 +46,7 @@ USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+    for x in (os.environ.get("ADMINS", "6909056240").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
